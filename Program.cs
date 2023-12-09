c96ae2e4378521017f32c8366ad58530fdbbe6d5
kodknackare <4346817+knackare@users.noreply.github.com>
2023-12-09 22:57:28 +0100

    Day 4

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using aoc2023;
 
-var solution = d3.solve();
+var solution = d4.solve();
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine(solution);
